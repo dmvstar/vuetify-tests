040adf5189dd6e78b3c7a5a0d1d06d5b1dde81c6
Dmitri Starzyński <dmitri.starzynski@benson.local>
2025-04-23 11:55:43 +0200

    Add logos+

--- src/nodered/test/graphhopper-1.sh
+++
@@ -1,9 +0,0 @@
-API_KEY="62080b6b-6c7f-4a97-98fe-cf357536c1e3"
-OFILE="$0".json
-DFILE=`basename $0 .sh`.json
-
-curl -w "@curl-format.json" -X POST -H "Content-Type: application/json" \
--s -d "@$DFILE" \
-"https://graphhopper.com/api/1/route?key=$API_KEY" | jq "."
-
-
