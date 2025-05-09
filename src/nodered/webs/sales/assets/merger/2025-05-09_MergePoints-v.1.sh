7a42159a296d7c94496cef9196bef45482944680
Dmitri Starzyński <dmitri.starzynski@benson.local>
2025-05-09 13:24:42 +0200

    add assets for merge

---
+++ src/nodered/webs/sales/assets/merger/2025-05-09_MergePoints-v.1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CFROM="#0000FF";
+
+COLORS=('#3b82f6' '#22c55e' '#eab308' '#06b6d4' '#ec4899' '#6366f1' '#14b8a6' '#f97316' '#a855f7' '#ff3d32' '#85b2f9' '#76db9b' '#f2d066' '#65d2e4' '#f38ec0' '#9ea0f6' '#6dd3c8' '#fba86f' '#c996fa' '#ff8780');
+IMAGES=('arrow_DN.png' 'arrow_UP.png' 'i_HomeB.png' 'p_BB.png');
+PIESKA='p_BB.png';
+IMAGES=('arrow_DN.png' 'arrow_UP.png' 'i_HomeB.png');
+
+#mogrify -path /fullpath2/tmp2 -format png -fill "#BFBFBF" -opaque "#C0C0C0" *.png
+#merge 
+#convert * -fill ${color} +opaque ${CFROM}
+for color in "${COLORS[@]}"; do
+    for image in "${IMAGES[@]}"; do
+        f_image=$(basename ${image} .png);
+        f_PIESKA=$(basename ${PIESKA} .png);
+        O_Image="${f_PIESKA}_${f_image}_${color}.png";
+        echo "Convert To ${O_Image}";
+    done    
+done
