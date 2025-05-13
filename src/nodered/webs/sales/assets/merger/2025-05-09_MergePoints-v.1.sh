0e353584cc0409c46ac6c98a4817c96f3887b1b1
Dmitri Starzyński <dmitri.starzynski@benson.local>
2025-05-13 10:33:18 +0200

    add cmd for merge ext all

--- src/nodered/webs/sales/assets/merger/2025-05-09_MergePoints-v.1.sh
+++ src/nodered/webs/sales/assets/merger/2025-05-09_MergePoints-v.1.sh
@@ -5,7 +5,8 @@ COLORS=('#3b82f6' '#22c55e' '#eab308' '#06b6d4' '#ec4899' '#6366f1' '#14b8a6' '#
 
 
 IMAGES=('arrow_DN.png' 'arrow_UP.png' 'i_HomeB.png' 'p_BB.png');
-PIESKA='p_BB.png';
+PIESKA='p_BB.png'; PIESKA_36='p_BB_36.png';
+POINTS=('p_RED.png' 'p_BLACK.png' 'p_DRED.png' 'p_BLUE.png' 'p_GREEN.png' 'p_ORANGE.png' 'p_LBLUE.png' 'p_PINK.png');
 IMAGES0=('arrow_DN.png' 'arrow_UP.png' 'i_HomeB.png');
 IMAGES=('i_Home_B.png' 'i_Arrow_UP_B.png' 'i_Arrow_DN_B.png' 'i_Car_B.png' 'i_Gear_B.png' 'i_S_B.png' 'i_Air_B.png' 'i_Bank_B.png' 'i_Bags_B.png' 'i_Park_B.png');
 
@@ -15,12 +16,19 @@ IMAGES=('i_Home_B.png' 'i_Arrow_UP_B.png' 'i_Arrow_DN_B.png' 'i_Car_B.png' 'i_Ge
 #magick output1.png -fill ${color} +opaque ${CFROM}
 #magick output1.png -fill "#ec4899" +opaque "#0000FF"
 
+cp in/${PIESKA} out/
+magick in/${PIESKA} -resize 36x out/${PIESKA_36}
 for image in "${IMAGES[@]}"; do        
     f_PIESKA=$(basename ${PIESKA} .png);
     f_image=$(basename ${image} .png);    
     N_Image="out/${f_PIESKA}_${f_image}.png";
+        point='p_RED.png'
+        p_point='p_RED'
+    P_Image="out/${f_PIESKA}_${f_image}_${p_point}.png";
+    
+    magick in/${PIESKA} in/${image} in/Punkty/${point} -composite ${P_Image}
     echo "Prepare ${N_Image}";
-    magick in/${PIESKA} in/${image} -composite out/${f_PIESKA}_${f_image}.png
+    magick in/${PIESKA} in/${image} -composite ${N_Image}
     for color in "${COLORS[@]}"; do
         f_color=$(echo ${color} | sed 's:#::g');
         O_Image="out/${f_PIESKA}_${f_image}_${f_color}.png";
