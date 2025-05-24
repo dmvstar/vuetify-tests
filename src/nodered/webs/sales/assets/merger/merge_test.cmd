8ef68e5dadc7baff6b0e82dce77bb9247137de74
stardust <dmvstar.devel@gmail.com>
2025-05-24 18:00:13 +0200

    updtae simple vuetify

---
+++ src/nodered/webs/sales/assets/merger/merge_test.cmd
@@ -0,0 +1,3 @@
+REM C:\home\apps\imagemagick\magick.exe p_BB.png i_HomeB.png -composite output1.png
+C:\home\apps\imagemagick\magick convert output1.png -fill "#ec4899" -opaque "#0000FF" output2.png
+
