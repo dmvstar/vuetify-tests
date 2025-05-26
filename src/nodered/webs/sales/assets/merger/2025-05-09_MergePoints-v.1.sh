35c74fc4b9f835ffabd19a0ab19758d0f8a9cac2
Dmitri Starzyński <dmitri.starzynski@benson.local>
2025-05-26 15:57:48 +0200

    update vue example, refactoring

--- src/nodered/webs/sales/assets/merger/2025-05-09_MergePoints-v.1.sh
+++ src/nodered/webs/sales/assets/merger/2025-05-09_MergePoints-v.1.sh
@@ -5,7 +5,7 @@ COLORS=('0000ff' '00ff00' 'ff0000');
 COLORS=('3b82f6' '22c55e' 'eab308' '06b6d4' 'ec4899' '6366f1' '14b8a6' 'f97316' 'a855f7' 'ff3d32' '85b2f9' '76db9b' 'f2d066' '65d2e4' 'f38ec0' '9ea0f6' '6dd3c8' 'fba86f' 'c996fa' 'ff8780');
 
 DIR_IN="in/pins"
-DIR_OUT="out"
+DIR_OUT="../../../../../../../out"
 
 PIESKA='p_BB.png';   PIESKA_36='p_BB.x36.png';
 PIESKA_O='p.png'; PIESKA_O_36='p.x36.png';
@@ -28,8 +28,8 @@ echo "Prepare points";
 for point in "pt_0000ff.png"; do
     f_POINT=$(basename ${point} .png);    
     for color in "${POINTS_COLORS[@]}"; do
-        fc_POINT="out/pt-${color}.png"
-        fc_POINT_32="out/pt-${color}.32.png"
+        fc_POINT="${DIR_OUT}/pt-${color}.png"
+        fc_POINT_32="${DIR_OUT}/pt-${color}.32.png"
         echo "  Prepare point ${fc_POINT}";
         magick ${DIR_IN}/points/${point} -fill "#${color}" -opaque "${CFROM}" ${fc_POINT}
         magick ${fc_POINT} -resize 32x ${fc_POINT_32}
@@ -39,8 +39,8 @@ done
 for image in "${IMAGES[@]}"; do
     f_PIESKA=$(basename ${PIESKA} .png);
     f_image=$(basename ${image} .png);
-    N_Image="out/${f_PIESKA}.${f_image}.pt-000000.png";
-    N_Image="out/p_${f_image}.pt-000000.png";
+    N_Image="${DIR_OUT}/${f_PIESKA}.${f_image}.pt-000000.png";
+    N_Image="${DIR_OUT}/p_${f_image}.pt-000000.png";
 
     echo "Prepare ${N_Image}";    
     #Add icon
@@ -48,10 +48,10 @@ for image in "${IMAGES[@]}"; do
 
     for color in "${COLORS[@]}"; do
         f_color=$(echo ${color} | sed 's:#::g');
-        O_Image="out/${f_PIESKA}.${f_image}-${f_color}.pt-000000.png";
-        S_Image="out/${f_PIESKA}.${f_image}-${f_color}.pt-000000.x32.png";
-        O_Image="out/p_${f_image}-${f_color}.pt-000000.png";
-        S_Image="out/p_${f_image}-${f_color}.pt-000000.x32.png";
+        O_Image="${DIR_OUT}/${f_PIESKA}.${f_image}-${f_color}.pt-000000.png";
+        S_Image="${DIR_OUT}/${f_PIESKA}.${f_image}-${f_color}.pt-000000.x32.png";
+        O_Image="${DIR_OUT}/p_${f_image}-${f_color}.pt-000000.png";
+        S_Image="${DIR_OUT}/p_${f_image}-${f_color}.pt-000000.x32.png";
         echo "  Full color To ${O_Image}";
         magick ${N_Image} -fill "#${color}" -opaque "${CFROM}" ${O_Image}
         #resize
@@ -60,12 +60,12 @@ for image in "${IMAGES[@]}"; do
         for point in "${POINTS[@]}"; do
             f_POINT=$(basename ${point} .png);
             f_POINT_32=${f_POINT}"_x32.png";
-            P_Image="out/${f_PIESKA}.${f_image}-${f_color}.${f_POINT}.png";
-            P_Image_32="out/${f_PIESKA}.${f_image}.${f_color}.${f_POINT}.x32.png";
-            P_Image="out/p_${f_image}-${f_color}.${f_POINT}.png";
-            P_Image_32="out/p_${f_image}-${f_color}.${f_POINT}.x32.png";
+            P_Image="${DIR_OUT}/${f_PIESKA}.${f_image}-${f_color}.${f_POINT}.png";
+            P_Image_32="${DIR_OUT}/${f_PIESKA}.${f_image}.${f_color}.${f_POINT}.x32.png";
+            P_Image="${DIR_OUT}/p_${f_image}-${f_color}.${f_POINT}.png";
+            P_Image_32="${DIR_OUT}/p_${f_image}-${f_color}.${f_POINT}.x32.png";
             echo "          Add point ${point} To ${P_Image}";
-            magick out/${point} ${O_Image} -composite ${P_Image}
+            magick ${DIR_OUT}/${point} ${O_Image} -composite ${P_Image}
             #resize
             magick ${P_Image} -resize 32x ${P_Image_32}
         done
