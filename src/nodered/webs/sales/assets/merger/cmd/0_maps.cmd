4d4adcae808962f2d70c15731534d6b4eafade6e
StarDust <dmvstar.devel@gmail.com>
2025-06-30 13:05:06 +0200

    add mapper cmd

---
+++ src/nodered/webs/sales/assets/merger/cmd/0_maps.cmd
@@ -0,0 +1,111 @@
+copy ..\pin\p_B.png                 p_B.png
+copy ..\pin\pt-ffff00.32.png        -p_ORANGE.png
+copy ..\pin\pt-0000ff.32.png        -p_BLUE.png
+copy ..\pin\pt-ff0000.32.png        -p_RED.png
+copy ..\pin\pt-00ff00.32.png        -p_GREEN.png
+copy ..\pin\pt-0000ff.32.png        -p_LBLUE.png
+copy ..\pin\pt-ff0000.32.png        -p_PINK.png
+
+
+copy ..\pin\p_i_Arrow_DN_B.pt-000000.png    p_BB_arrow_DN.png
+copy ..\pin\p_i_Arrow_UP_B.pt-000000.png    p_BB_arrow_UP.png
+
+
+
+
+
+copy ..\pin\p_B.png                 p_B.png
+copy ..\pin\pt-ffff00.32.png                 -p_ORANGE.png
+copy ..\pin\pt-0000ff.32.png                 -p_BLUE.png
+copy ..\pin\pt-ff0000.32.png                 -p_RED.png
+copy ..\pin\pt-00ff00.32.png                 -p_GREEN.png
+copy ..\pin\pt-0000ff.32.png                 -p_LBLUE.png
+copy ..\pin\pt-ff0000.32.png                 -p_PINK.png
+
+copy ..\pin\p_i_Arrow_DN_B.pt-000000.png                 p_BB_arrow_DN.png
+copy ..\pin\p_i_Arrow_UP_B.pt-000000.png                 p_BB_arrow_UP.png
+
+copy ..\pin\p_i_Air_B-0000ff.pt-ff0000.png                 i_AirL_M-p_ORANGE.png
+copy ..\pin\p_i_Air_B-0000ff.pt-ff0000.png                 i_AirL_M-p_RED.png
+copy ..\pin\p_i_Air_B-0000ff.pt-000000.png                 i_AirL_M.png
+copy ..\pin\p_i_Bags_B-0000ff.pt-ff0000.png                 i_Bags_M-p_ORANGE.png
+copy ..\pin\p_i_Bags_B-0000ff.pt-ff0000.png                 i_Bags_M-p_RED.png
+copy ..\pin\p_i_Bags_B-0000ff.pt-000000.png                 i_Bags_M.png
+copy ..\pin\p_i_Bank_B-0000ff.pt-ff0000.png                 i_Bank_M-p_ORANGE.png
+copy ..\pin\p_i_Bank_B-0000ff.pt-ff0000.png                 i_Bank_M-p_RED.png
+copy ..\pin\p_i_Bank_B-0000ff.pt-000000.png                 i_Bank_M.png
+copy ..\pin\p_i_Car_B-0000ff.pt-ff0000.png                 i_CarB_M-p_ORANGE.png
+copy ..\pin\p_i_Car_B-0000ff.pt-ff0000.png                 i_CarB_M-p_RED.png
+copy ..\pin\p_i_Car_B-0000ff.pt-000000.png                 i_CarB_M.png
+copy ..\pin\p_i_Dollar_B-00ff00.pt-ff0000.png                 i_DollarG_M-p_ORANGE.png
+copy ..\pin\p_i_Dollar_B-00ff00.pt-ff0000.png                 i_DollarG_M-p_RED.png
+copy ..\pin\p_i_Dollar_B-00ff00.pt-000000.png                 i_DollarG_M.png
+copy ..\pin\p_i_Gear_B-0000ff.pt-ff0000.png                 i_GearB_M-p_ORANGE.png
+copy ..\pin\p_i_Gear_B-0000ff.pt-ff0000.png                 i_GearB_M-p_RED.png
+copy ..\pin\p_i_Gear_B-0000ff.pt-000000.png                 i_GearB_M.png
+copy ..\pin\p_i_Gear_B-00ff00.pt-ff0000.png                 i_GearG_M-p_ORANGE.png
+copy ..\pin\p_i_Gear_B-00ff00.pt-ff0000.png                 i_GearG_M-p_RED.png
+copy ..\pin\p_i_Gear_B-00ff00.pt-000000.png                 i_GearG_M.png
+copy ..\pin\p_i_Home_B-0000ff.pt-ff0000.png                 i_HomeB_M-p_ORANGE.png
+copy ..\pin\p_i_Home_B-0000ff.pt-ff0000.png                 i_HomeB_M-p_RED.png
+copy ..\pin\p_i_Home_B-0000ff.pt-000000.png                 i_HomeB_M.png
+copy ..\pin\p_i_Home_B-00ff00.pt-ff0000.png                 i_HomeG_M-p_ORANGE.png
+copy ..\pin\p_i_Home_B-00ff00.pt-ff0000.png                 i_HomeG_M-p_RED.png
+copy ..\pin\p_i_Home_B-00ff00.pt-000000.png                 i_HomeG_M.png
+copy ..\pin\p_i_Home_B-ff0000.pt-ff0000.png                 i_HomeR_M-p_ORANGE.png
+copy ..\pin\p_i_Home_B-ff0000.pt-ff0000.png                 i_HomeR_M-p_RED.png
+copy ..\pin\p_i_Home_B-ff0000.pt-000000.png                 i_HomeR_M.png
+copy ..\pin\p_i_Park_B-0000ff.pt-ff0000.png                 i_ParkB_M-p_ORANGE.png
+copy ..\pin\p_i_Park_B-0000ff.pt-ff0000.png                 i_ParkB_M.png
+copy ..\pin\p_i_Park_B-0000ff.pt-000000.png                 i_Parking_M-p_RED.png
+copy ..\pin\p_i_Park_B-ff0000.pt-ff0000.png                 i_ParkO_M-p_ORANGE.png
+copy ..\pin\p_i_Park_B-ff0000.pt-ff0000.png                 i_ParkO_M-p_RED.png
+copy ..\pin\p_i_Park_B-ff0000.pt-000000.png                 i_ParkO_M.png
+
+copy ..\pin\p_i_Arrow_DN_B-06b6d4.pt-000000.png                 p_BB_arrow_DN_06b6d4.png
+copy ..\pin\p_i_Arrow_DN_B-14b8a6.pt-000000.png                 p_BB_arrow_DN_14b8a6.png
+copy ..\pin\p_i_Arrow_DN_B-22c55e.pt-000000.png                 p_BB_arrow_DN_22c55e.png
+copy ..\pin\p_i_Arrow_DN_B-3b82f6.pt-000000.png                 p_BB_arrow_DN_3b82f6.png
+copy ..\pin\p_i_Arrow_DN_B-6366f1.pt-000000.png                 p_BB_arrow_DN_6366f1.png
+copy ..\pin\p_i_Arrow_DN_B-65d2e4.pt-000000.png                 p_BB_arrow_DN_65d2e4.png
+copy ..\pin\p_i_Arrow_DN_B-6dd3c8.pt-000000.png                 p_BB_arrow_DN_6dd3c8.png
+copy ..\pin\p_i_Arrow_DN_B-76db9b.pt-000000.png                 p_BB_arrow_DN_76db9b.png
+copy ..\pin\p_i_Arrow_DN_B-85b2f9.pt-000000.png                 p_BB_arrow_DN_85b2f9.png
+copy ..\pin\p_i_Arrow_DN_B-9ea0f6.pt-000000.png                 p_BB_arrow_DN_9ea0f6.png
+copy ..\pin\p_i_Arrow_DN_B-a855f7.pt-000000.png                 p_BB_arrow_DN_a855f7.png
+copy ..\pin\p_i_Arrow_DN_B-c996fa.pt-000000.png                 p_BB_arrow_DN_c996fa.png
+copy ..\pin\p_i_Arrow_DN_B-eab308.pt-000000.png                 p_BB_arrow_DN_eab308.png
+copy ..\pin\p_i_Arrow_DN_B-ec4899.pt-000000.png                 p_BB_arrow_DN_ec4899.png
+copy ..\pin\p_i_Arrow_DN_B-f2d066.pt-000000.png                 p_BB_arrow_DN_f2d066.png
+copy ..\pin\p_i_Arrow_DN_B-f38ec0.pt-000000.png                 p_BB_arrow_DN_f38ec0.png
+copy ..\pin\p_i_Arrow_DN_B-f97316.pt-000000.png                 p_BB_arrow_DN_f97316.png
+copy ..\pin\p_i_Arrow_DN_B-fba86f.pt-000000.png                 p_BB_arrow_DN_fba86f.png
+copy ..\pin\p_i_Arrow_DN_B-ff3d32.pt-000000.png                 p_BB_arrow_DN_ff3d32.png
+copy ..\pin\p_i_Arrow_DN_B-ff8780.pt-000000.png                 p_BB_arrow_DN_ff8780.png
+
+copy ..\pin\p_i_Arrow_UP_B-06b6d4.pt-000000.png                 p_BB_arrow_UP_06b6d4.png
+copy ..\pin\p_i_Arrow_UP_B-14b8a6.pt-000000.png                 p_BB_arrow_UP_14b8a6.png
+copy ..\pin\p_i_Arrow_UP_B-22c55e.pt-000000.png                 p_BB_arrow_UP_22c55e.png
+copy ..\pin\p_i_Arrow_UP_B-3b82f6.pt-000000.png                 p_BB_arrow_UP_3b82f6.png
+copy ..\pin\p_i_Arrow_UP_B-6366f1.pt-000000.png                 p_BB_arrow_UP_6366f1.png
+copy ..\pin\p_i_Arrow_UP_B-65d2e4.pt-000000.png                 p_BB_arrow_UP_65d2e4.png
+copy ..\pin\p_i_Arrow_UP_B-6dd3c8.pt-000000.png                 p_BB_arrow_UP_6dd3c8.png
+copy ..\pin\p_i_Arrow_UP_B-76db9b.pt-000000.png                 p_BB_arrow_UP_76db9b.png
+copy ..\pin\p_i_Arrow_UP_B-85b2f9.pt-000000.png                 p_BB_arrow_UP_85b2f9.png
+copy ..\pin\p_i_Arrow_UP_B-9ea0f6.pt-000000.png                 p_BB_arrow_UP_9ea0f6.png
+copy ..\pin\p_i_Arrow_UP_B-a855f7.pt-000000.png                 p_BB_arrow_UP_a855f7.png
+copy ..\pin\p_i_Arrow_UP_B-c996fa.pt-000000.png                 p_BB_arrow_UP_c996fa.png
+copy ..\pin\p_i_Arrow_UP_B-eab308.pt-000000.png                 p_BB_arrow_UP_eab308.png
+copy ..\pin\p_i_Arrow_UP_B-ec4899.pt-000000.png                 p_BB_arrow_UP_ec4899.png
+copy ..\pin\p_i_Arrow_UP_B-f2d066.pt-000000.png                 p_BB_arrow_UP_f2d066.png
+copy ..\pin\p_i_Arrow_UP_B-f38ec0.pt-000000.png                 p_BB_arrow_UP_f38ec0.png
+copy ..\pin\p_i_Arrow_UP_B-f97316.pt-000000.png                 p_BB_arrow_UP_f97316.png
+copy ..\pin\p_i_Arrow_UP_B-fba86f.pt-000000.png                 p_BB_arrow_UP_fba86f.png
+copy ..\pin\p_i_Arrow_UP_B-ff3d32.pt-000000.png                 p_BB_arrow_UP_ff3d32.png
+copy ..\pin\p_i_Arrow_UP_B-ff8780.pt-000000.png                 p_BB_arrow_UP_ff8780.png
+
+
+
+
+
+pause
